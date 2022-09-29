a48d603bd00436be804b68711b1aedab8b7e9541
anwjrrp33 <anwjrrp33@gmail.com>
2022-09-29 22:48:28 +0900

    feat: 볼, 스트라이크, 낫싱 결과값 출력 기능

--- src/main/java/baseball/code/DisplayBoardCode.java
+++ src/main/java/baseball/code/DisplayBoardCode.java
@@ -2,6 +2,12 @@ package baseball.code;
 
 public enum DisplayBoardCode {
 
+    BALL("볼"),
+
+    STRIKE("스트라이크"),
+
+    NOTHING("낫싱"),
+
     INPUT("숫자를 입력해주세요 : ");
 
     private String message;
--- src/main/java/baseball/view/DisplayBoard.java
+++ src/main/java/baseball/view/DisplayBoard.java
@@ -1,10 +1,34 @@
 package baseball.view;
 
 import baseball.code.DisplayBoardCode;
+import baseball.model.Count;
 
 public class DisplayBoard {
 
     public static void input() {
         System.out.print(DisplayBoardCode.INPUT.getMessage());
+    }
+
+    public static void count(Count count) {
+        if(!count.isNothing()) {
+            resultCount(count);
+            return;
+        }
+        System.out.println(DisplayBoardCode.NOTHING.getMessage());
+    }
+
+    private static void resultCount(Count count) {
+        StringBuilder st = new StringBuilder();
+        st.append(zeroCheck(count.getBall(), DisplayBoardCode.BALL.getMessage()));
+        st.append(st.length() > 0 ? " " : "");
+        st.append(zeroCheck(count.getStrike(), DisplayBoardCode.STRIKE.getMessage()));
+        System.out.println(st);
+    }
+
+    private static String zeroCheck(int count, String message) {
+        if(count > 0) {
+            return count + message;
+        }
+        return "";
     }
 }
